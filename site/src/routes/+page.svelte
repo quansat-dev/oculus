8d96fe837e425f0a83aafd0d7e3632369583230b
Quang Phan <vnphanquang@gmail.com>
2024-12-07 11:50:53 +0700

    chore: set up a barebone homepage

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	// import plate01 from '@quansat/oculus/ishihara/plate-01.jpg?enhanced';
-</script>
-
-<main class="grid h-screen w-screen place-items-center">
-	<!-- <enhanced:img class="w-80 h-80 border-2" src={plate01} /> -->
+<main class="grid h-screen w-screen place-items-center bg-stone-900 text-stone-50">
+	<h1 class="text-6xl">Quan sát &nbsp;⦿&nbsp; Oculus</h1>
 </main>
